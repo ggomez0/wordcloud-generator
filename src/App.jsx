fc199598d32ebf8cb62da221bde903c69c56f402
ggomez0 <gaspargomez2000@outlook.com>
2024-01-14 13:36:51 -0300

    images on vercel

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
     if (dateValue) {
       const formattedDate = new Date(dateValue).toISOString().slice(0, 10);
       document.getElementById("fecha_actual").textContent = formattedDate;
-      setWordcloudImage("wordcloud/Wordcloud - " + formattedDate + ".png");
+      setWordcloudImage("/wordcloud/Wordcloud - " + formattedDate + ".png");
     }
   };
   return (
